4be91a078d72d9610e2f57fd93689ade0cc2ea48
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-12-03 14:23:37 -0800

    footer functional

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
+import gitHubImage from "../assets/ghImage.png"
+import linkedInImage from "../assets/linkedIn.png"
 
 
 function footer() {
   return (
-    <div>
-        <h1>MY FOOTER</h1>
+    <div className="footerLinks">
+        <a target="_blank" rel="noreferrer" href="https://www.github.com/NathanAlexander1"><img src={gitHubImage} height="40" alt="GitHub Link"/></a>
+        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/nathan-alexander-a8460713a"><img src={linkedInImage} height="40" alt="LinkedIn Link"/></a>
     </div>
   )
 }
