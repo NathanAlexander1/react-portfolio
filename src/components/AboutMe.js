0135c9a4533bb925047bf6187ae46effd906f377
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-12-03 23:57:00 -0800

    resume section complete

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,7 @@ function AboutMe() {
     <div>
       <h1>About me</h1>
       <img src={nathanAtMeru} height="200" alt="Nathan at Meru"></img>
-      <p>Hello! My name is Nathan Alexander and I am a full stack software engineer. I recently earned my <a target="_blank" href="https://bootcamp.uw.edu/coding/"> cerificate in full stack development</a> from the University of Washington's coding bootcamp and am eager to dive into the field of web development!</p>
+      <p>Hello! My name is Nathan Alexander and I am a full stack software engineer. I recently earned my <a target="_blank" rel="noreferrer" href="https://bootcamp.uw.edu/coding/"> cerificate in full stack development</a> from the University of Washington's coding bootcamp and am eager to dive into the field of web development!</p>
       
       <p>Prior to shifting my career towards tech, I worked in the non-profit sector. Most recently, I worked for the Humane Society of the United States, helping to better our food system to improve human health, animal welfare and the wellbeing of the planet.</p>
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,16 +1,44 @@
-import React from 'react'
-import NathanResume from "../assets/Nathan-Resume.pdf"
+import React from "react";
+import NathanResume from "../assets/Nathan-Resume.pdf";
+import HTML from "../assets/proficiencies/html.png"
+import CSS from "../assets/proficiencies/css.png"
+import JSLogo from "../assets/proficiencies/jslogo.png"
+import reactLogo from "../assets/proficiencies/reactlogo.png"
+import node from "../assets/proficiencies/nodelogo.webp"
 
 function Resume() {
   return (
-    <div>
+    <div className="resumeDiv">
       <h1>Download my resume below</h1>
-      <p>RESUME PLACEHOLDER</p>
-
-      <a href={NathanResume} download>Download my resume</a>
+      <a href={NathanResume} download>
+        Download my resume
+      </a>
       <p>Proficiencies</p>
+      <div className="proficiencies">
+        <div>
+          <img src={HTML} alt="html logo" height="80"/>
+          <p>HTML</p>
+        </div>
+        <div>
+        <img src={CSS} alt="css logo" height="80"/>
+          <p>CSS</p>
+        </div>
+        <div>
+        <img src={JSLogo} alt="javascript logo" height="80"/>
+          <p>JavaScript</p>
+        </div>
+        <div>
+        <img src={reactLogo} alt="react logo" height="80"/>
+          <p>React.js</p>
+        </div>
+        <div>
+        <img src={node} alt="node logo" height="80"/>
+          <p>Node.js</p>
+        </div>
+      </div>
+      <p>I am also proficient in ....................</p>
     </div>
-  )
+  );
 }
 
-export default Resume
+export default Resume;
